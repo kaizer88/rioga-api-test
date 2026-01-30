809f6518de74107dc4dec3f6395f32d7f1c544f7
kaizer88 <ltmagwentshu@gmail.com>
2026-01-30 20:29:31 +0200

    setup

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/api/hello")
 def hello_world():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello,  fastWorld!"}
 
 if __name__ == "__main__":
     import uvicorn
